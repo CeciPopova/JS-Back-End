1de5f2869ea8b8340e6755401f65c9d71345b354
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2023-05-20 00:03:54 +0300

    Modified Files

--- 01. Intro to Node.js/Cat-Shelter/index.js
+++ 01. Intro to Node.js/Cat-Shelter/index.js
@@ -1,61 +1,60 @@
 const http = require('http');
-const homeHtml = require('./views/home/index');
+const fs = require('fs/promises');
 const siteCss = require('./content/styles/site');
-const addBreedHtml = require('./views/addBreed');
 
-cats = [
+const cats = [
     {
         id: 1,
         name: 'Navcho',
         breed: 'Persian',
-        description: 'Very cute cat'
+        description: 'Very cute cat',
     },
     {
         id: 2,
         name: 'Mishi',
         breed: 'Angora',
-        description: 'Fluffy cat'
+        description: 'Fluffy cat',
     },
     {
         id: 3,
         name: 'Garry',
         breed: 'Angora',
-        description: 'Fat cat'
-    }
+        description: 'Fat cat',
+    },
 ];
-const catTemplate = ` 
-<li>
-   <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg" alt="Black Cat">
-   <h3></h3>
-   <p><span>Breed: </span>Bombay Cat</p>
-   <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
-   <ul class="buttons">
-     <li class="btn edit"><a href="">Change Info</a></li>
-     <li class="btn delete"><a href="">New Home</a></li>
-   </ul>
-</li>`;
+
+const replaceData = (html, data) => {
+    return Object.keys(data).reduce((result, key) => {
+        result = result.replace(`{{${key}}}`, data[key])
+
+        return result;
+    }, html)
+};
 
 const server = http.createServer(async (req, res) => {
-const url = req.url;
-if (url == '/') {
-    res.writeHead(200, {
-                   //mime type
-        'Content-Type': 'text/html'
-    });
-    res.write(homeHtml);
-}else if (url == '/content/styles/site.css'){
-    res.writeHead(200, {
-        'Content-Type': 'text/css'
-    });
-    res.write(siteCss);
-}else if (url == '/cats/add-breed'){
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    res.write(addBreedHtml);
-}
+    console.log(req.url);
 
-res.end();
+    if (req.url == '/') {
+        const homeHtml = await fs.readFile('./views/home/index.html', 'utf-8');
+        const catHtml = await fs.readFile('./views/cat.html', 'utf-8'); 
+
+        const catsHtml = cats.map(cat => replaceData(catHtml, cat));
+
+        const homeResult = replaceData(homeHtml, {cats: catsHtml})
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(homeResult);
+    } else if (req.url == '/styles/site.css') {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(siteCss);
+    } else if (req.url == '/cats/add-breed') {
+        const addBreedHtml = await fs.readFile('./views/addBreed.html', 'utf-8');
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(addBreedHtml);
+    }
+
+    res.end();
 });
 
-server.listen(5000, () => console.log('Server is running on port 5000...'));
+server.listen(5000, () => console.log('This server is running on port 5000...'));
