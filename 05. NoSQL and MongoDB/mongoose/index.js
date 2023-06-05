66e15f22fb0e83e4cacd0466e37e114d64a37460
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-06-05 11:31:13 +0300

    Modified Folder

--- 05. NoSQL and MongoDB/mongoose/index.js
+++ 05. NoSQL and MongoDB/mongoose/index.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
+const Cat = require('./models/Cat');
 
  async function connectDb() {
-    await mongoose.connect('mongodb://127.0.0.1.27017/aCt-Shelter');
+    await mongoose.connect('mongodb://127.0.0.1:27017/catShelter');
     console.log('Successfully connected!');
+
+    const cats = await Cat.find({ breed: 'Angora'});
+
+    cats.forEach(cat => cat.greet());
+    console.log(cats);
 }
 
 connectDb();
